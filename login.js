d2c9f267094abaac646131e0bbc73f004242c9ae
AngelZamoran019 <azamorano017@gmail.com>
2026-09-10 23:29:03 -0600

    Change local login password in login.js
    Updated the local login password for development.

--- login.js
+++ login.js
@@ -4,7 +4,7 @@ const LOGIN_USER = "dangelsps";
  * Local development only.
  * Set the password in your local copy. Do not commit the real password.
  */
-const LOGIN_PASSWORD = "CAMBIA_ESTA_CONTRASENA_LOCALMENTE";
+const LOGIN_PASSWORD = "dangelspsz1";
 const SESSION_KEY = "dangels_print_studio_authenticated";
 
 document.getElementById("loginForm").addEventListener("submit", function (event) {
